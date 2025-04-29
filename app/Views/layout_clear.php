790a1f1922388c9cc2c77d5510c91ee92e9544c3
El-PanjiPenghancurDunia <panjikusumo89@gmail.com>
2025-04-29 18:40:58 +0700

    revisi 4

--- app/Views/layout_clear.php
+++ app/Views/layout_clear.php
@@ -1,5 +1,5 @@
 <?php
-$hlm = "Home";
+$hlm = "Login";
 if(uri_string()!=""){
   $hlm = ucwords(uri_string());
 }
@@ -11,13 +11,13 @@ if(uri_string()!=""){
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>- Toko - <?php echo $hlm ?></title>
+  <title>- Warung - <?php echo $hlm ?></title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="<?= base_url()?>NiceAdmin/assets/img/favicon.png" rel="icon">
-  <link href="<?= base_url()?>NiceAdmin/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="<?= base_url()?>NiceAdmin/assets/img/broccoli.png" rel="icon">
+  <link href="<?= base_url()?>NiceAdmin/assets/img/broccoli.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
